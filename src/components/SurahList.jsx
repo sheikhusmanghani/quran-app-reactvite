d1ebb7112f2c8f079c2826784695300a0b0e2109
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2025-02-14 16:01:20 +0500

    color changed

--- src/components/SurahList.jsx
+++ src/components/SurahList.jsx
@@ -65,7 +65,7 @@ export default function SurahList() {
           //   </span>
           // </div>
           <div className="flex items-center justify-center min-h-[50vh]">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-4 mainColor"></div>
+            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
           </div>
         ) : (
           <>
